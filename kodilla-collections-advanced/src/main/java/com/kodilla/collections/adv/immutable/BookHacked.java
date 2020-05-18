471fdea2e0cabe5284aecebd46f6ef04bee4992e
Damian Arciszewski <damian.arciszewski@gmail.com>
2020-05-18 12:09:12 +0200

    Mod 5.3 Niemutowalnosc

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/BookHacked.java
@@ -0,0 +1,15 @@
+package com.kodilla.collections.adv.immutable;
+
+class BookHacked extends Book {
+    public BookHacked(String author, String title) {
+        super(author, title);
+
+    }
+
+    public void modifyTitle(String newTitle) {
+        title = newTitle;
+    }
+    public void modifyAuthor(String newAuthor) {
+        author = newAuthor;
+    }
+}
